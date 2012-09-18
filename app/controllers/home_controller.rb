ebb8466d818169ed11c70725cfa851bb0aa75b41
andy <ejic045440@gmail.com>
2012-09-18 10:48:18 +0000

    ec2 first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+      @num = 10
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+M5::Application.config.secret_token = '354d7d4244f018105fdd08ee3464b21460a6e68f8c455ee97f4a6393a102ac7c6624845065a549d3b235fc2498d81f806386b8628f95f6286193f9f389684052'
